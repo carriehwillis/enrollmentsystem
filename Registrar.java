b9fc96ca51b7d114aaec0f4c39e5078a5fc3a6d9
Carrie Willis <chw675@ads.northwestern.edu>
2019-06-07 17:19:09 -0500

    Implemented name parser to deal with Enrollment.txt file name formatting

--- Registrar.java
+++ Registrar.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.util.Scanner;
+import java.io.*;
+import java.nio.file.*;
 
 /**
 *	A registrar system which creates courses and teachers based on user input.
@@ -10,7 +12,9 @@ import java.util.Scanner;
 public class Registrar
 {
 	private Scanner scanner;
-	private Database database;
+	private String fName;
+	private String middle;
+	private String lName;
 
 	/**
 	*	Create a registrar item.
@@ -18,22 +22,92 @@ public class Registrar
 	public Registrar()
 	{
 		scanner = new Scanner(System.in);
-		database = new Database();
 	}
 
-	public static void main()
+
+		// /**
+   // * Loads a list of word-response pairs into the response map
+   // * by reading from a text file.
+   // * @throws FileNotFoundException if the file is not found
+   // * @throws IOException if the file cannot be read.
+   // */
+  // public void loadResponseMap()
+  // {
+     // Charset charset = Charset.forName("US-ASCII");
+     // Path path = Paths.get(ENROLLMENT);
+     // try(BufferedReader reader = Files.newBufferedReader(path, charset))
+     // {
+         // String response = reader.readLine();
+         // while(response != null)
+         // {
+             // String response2 = reader.readLine();
+             // responseMap.put(response, response2);
+             // response = reader.readLine();
+         // }
+      // }
+      // catch(FileNotFoundException e) {
+          // System.err.println("Unable to open " + ENROLLMENT);
+      // }
+      // catch(IOException e) {
+          // System.err.println("A problem was encountered reading " +
+                             // ENROLLMENT);
+      // }
+  // }
+	public void parseNames(String line)
 	{
-		System.out.println("Welcome to the CAESAR+ Enrollment System.");
-		System.out.println("Please enter the information below to create a new course.");
-		System.out.println()
-		Course course1 = new Course();
-		System.out.println("Please enter the information below to add a new faculty member for this course.");
-		System.out.println();
-		Teacher prof1 = new Teacher();
-		database.addClass(course, prof);
-		database.printRoster(course1);
+		String[] parts = line.split(" ");
+		int size = parts.length;
+		for(String name : parts)
+		{
+			if(name.contains(","))
+			{
+				processCommaName(line);
+				break;
+			}
+			else if(size > 2)
+			{
+				fName = parts[0];
+				lName = parts[size - 1];
+				middle = parts[1];
+				for(int i=2; i <(size - 1); i++)
+				{
+					middle += " " + parts[i];
+				}
+			}
+			else
+			{
+				fName = parts[0];
+				middle = null;
+				lName = parts[1];
+			}
+		}
 	}
-}
 
+	private void processCommaName(String line)
+	{
+		String[] parts = line.split(" ");
+		int size = parts.length;
+		for(String name : parts)
+		{
+			if(name.contains(",") && parts[0] == name)
+			{
+				lName = name.substring(0, name.length()-1);
+			}
+		}
+		if(size == 2)
+		{
+			fName = parts[1];
+			middle = null;
+		}
+		if(size > 2)
+		{
+			fName = parts[1];
+			middle = parts[2];
+			for(int i=3; i < (size); i++)
+			{
+			   middle += parts[i] + " ";
+			}
+		}
+	}
 
 }
