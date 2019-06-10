105983a44bf1085bdc8348f0bee6061b39efbc7e
Carrie Willis <chw675@ads.northwestern.edu>
2019-06-09 19:22:02 -0500

    Changed main() to reflect new changes.

--- EnrollmentProgram.java
+++ EnrollmentProgram.java
@@ -1,5 +1,8 @@
 public class EnrollmentProgram
 {
+    private static CourseDatabase courseDB;
+    private static PeopleDatabase peopleDB;
+
     public EnrollmentProgram()
     {
 
@@ -7,6 +10,8 @@ public class EnrollmentProgram
 
     public static void main(String[] args)
     {
+      courseDB = new CourseDatabase();
+      peopleDB = new PeopleDatabase();
       System.out.println("Welcome to the CAESAR+ Enrollment System.");
       System.out.println("Please enter the information below to create a new course.");
       System.out.println();
@@ -14,7 +19,7 @@ public class EnrollmentProgram
       System.out.println("Please enter the information below to add a new faculty member for this course.");
       System.out.println();
       Teacher prof1 = new Teacher();
-      Database.addToClass(course1, prof1);
-      Database.printRoster(course1);
+      courseDB.addToClass(course1, prof1);
+      courseDB.printRoster(course1);
     }
 }
